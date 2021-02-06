ee3b2a245d0aa3ccdfa15446c80c9bb3177d32a5
dosbenjamin <dosbenjamin@gmail.com>
2021-02-06 17:29:07 +0100

    Add custom style to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,33 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      black: '#1F1C1C',
-      white: '#fff',
-      blue: '#5494F5',
-      grey: {
-        300: '#FBFBFB',
-        400: '#EBEBEB',
-        600: '#B5B5B5'
+      'black': '#1F1C1C',
+      'white': '#fff',
+      'blue': '#5494F5',
+      'grey': {
+        '300': '#FBFBFB',
+        '400': '#EBEBEB',
+        '600': '#B5B5B5'
       },
     },
+    fontSize: {
+      'small': '0.833rem',
+      'base': '1rem',
+      'lg': '1.728rem',
+      'xl': '2.074rem',
+      '2xl': '3.583rem'
+    },
     fontFamily: {
-      sans: ['Kumbh Sans', 'sans-serif']
+      'sans': ['Kumbh Sans', 'sans-serif']
+    },
+    extend: {
+      spacing: {
+        '1200': '75rem',
+        '760': '48rem'
+      },
+      borderWidth: {
+        '1': '1px'
+      }
     }
   },
   variants: {},
