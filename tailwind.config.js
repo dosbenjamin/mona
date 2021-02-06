6d049ff16a2220e98e7cde95ef0f0a4041f86643
dosbenjamin <dosbenjamin@gmail.com>
2021-02-06 15:23:11 +0100

    FIrst commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // purge: [
+  //   './src/**/*.html',
+  //   './src/**/*.js',
+  // ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      black: '#1F1C1C',
+      white: '#fff',
+      blue: '#5494F5',
+      grey: {
+        300: '#FBFBFB',
+        400: '#EBEBEB',
+        600: '#B5B5B5'
+      },
+    },
+    fontFamily: {
+      sans: ['Kumbh Sans', 'sans-serif']
+    }
+  },
+  variants: {},
+  plugins: [],
+}
