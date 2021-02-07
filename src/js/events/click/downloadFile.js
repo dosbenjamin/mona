556c7d6308ce6c14927c7336113c2f4a4bdee233
dosbenjamin <dosbenjamin@gmail.com>
2021-02-07 18:56:48 +0100

    Add bill download

---
+++ src/js/events/click/downloadFile.js
@@ -0,0 +1,44 @@
+import easyinvoice from 'easyinvoice'
+import 'regenerator-runtime/runtime'
+
+export default () => {
+  event.preventDefault()
+
+  const {
+    customer,
+    address,
+    zip,
+    city,
+    number,
+    firstname,
+    country,
+    lastname,
+    services,
+    date: { year, month, day }
+  } = JSON.parse(localStorage.getItem('saleBills'))[self.bill]
+
+  const data = {
+    'currency': '€',
+    'taxNotation': 'TVA',
+    'client': {
+      'company': customer,
+      'address': `${ address } ${ number }`,
+      'zip': zip,
+      'city': city,
+      'country': country
+    },
+    'invoiceNumber': self.bill,
+    'invoiceDate': `${ day } ${ month } ${ year }`,
+    'products': services
+  }
+
+  console.log('a')
+
+  easyinvoice.createInvoice(data, function (result) {
+    // easyinvoice.download('myInvoice.pdf')
+    console.log(result)
+    //	you can download like this as well:
+    //	easyinvoice.download();
+    //	easyinvoice.download('myInvoice.pdf');
+  })
+}
