3600b83f72a02044d5492f554d934e3bc5112062
dosbenjamin <dosbenjamin@gmail.com>
2021-02-08 21:28:41 +0100

    Remove easyinvoice

--- src/js/events/click/downloadFile.js
+++ src/js/events/click/downloadFile.js
@@ -2,43 +2,43 @@ import easyinvoice from 'easyinvoice'
 import 'regenerator-runtime/runtime'
 
 export default () => {
-  event.preventDefault()
+  // event.preventDefault()
 
-  const {
-    customer,
-    address,
-    zip,
-    city,
-    number,
-    firstname,
-    country,
-    lastname,
-    services,
-    date: { year, month, day }
-  } = JSON.parse(localStorage.getItem('saleBills'))[self.bill]
+  // const {
+  //   customer,
+  //   address,
+  //   zip,
+  //   city,
+  //   number,
+  //   firstname,
+  //   country,
+  //   lastname,
+  //   services,
+  //   date: { year, month, day }
+  // } = JSON.parse(localStorage.getItem('saleBills'))[self.bill]
 
-  const data = {
-    'currency': '€',
-    'taxNotation': 'TVA',
-    'client': {
-      'company': customer,
-      'address': `${ address } ${ number }`,
-      'zip': zip,
-      'city': city,
-      'country': country
-    },
-    'invoiceNumber': self.bill,
-    'invoiceDate': `${ day } ${ month } ${ year }`,
-    'products': services
-  }
+  // const data = {
+  //   'currency': '€',
+  //   'taxNotation': 'TVA',
+  //   'client': {
+  //     'company': customer,
+  //     'address': `${ address } ${ number }`,
+  //     'zip': zip,
+  //     'city': city,
+  //     'country': country
+  //   },
+  //   'invoiceNumber': self.bill,
+  //   'invoiceDate': `${ day } ${ month } ${ year }`,
+  //   'products': services
+  // }
 
-  console.log('a')
+  // console.log('a')
 
-  easyinvoice.createInvoice(data, function (result) {
-    // easyinvoice.download('myInvoice.pdf')
-    console.log(result)
-    //	you can download like this as well:
-    //	easyinvoice.download();
-    //	easyinvoice.download('myInvoice.pdf');
-  })
+  // easyinvoice.createInvoice(data, function (result) {
+  //   // easyinvoice.download('myInvoice.pdf')
+  //   console.log(result)
+  //   //	you can download like this as well:
+  //   //	easyinvoice.download();
+  //   //	easyinvoice.download('myInvoice.pdf');
+  // })
 }
