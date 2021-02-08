f23fea7d9b6a91184958a72533bb73de108513ac
dosbenjamin <dosbenjamin@gmail.com>
2021-02-08 21:22:10 +0100

    Refactor bill submiting with immutability

--- src/js/events/click/submitBill.js
+++ src/js/events/click/submitBill.js
@@ -6,14 +6,14 @@ const TVA = 21
 const $infos = $$('.js-contact-area .js-form-input')
 
 export default () => {
-  const bill = $infos.reduce((bill, $info) => {
-    bill[$info.name] = $info.value
-    return bill
-  }, {})
-
   const $services = $$('.js-services-area .js-form-input')
 
-  bill.services = $services
+  const customer = $infos.reduce((infos, $info) => {
+    infos[$info.name] = $info.value
+    return infos
+  }, {})
+
+  const services = $services
     .filter(($service, index) => !(index % 2))
     .map(($service, index) => {
       const hours = $('.js-form-input', $service.parentElement.nextElementSibling).value
@@ -24,19 +24,22 @@ export default () => {
         htva: RATE * hours,
         tvac: ((RATE * hours) * 0.21) + (RATE * hours)
       }
-  })
+    })
 
-  bill.htva = bill.services
+  const htva = services
     .map(({ htva }) => htva)
     .reduce((total, amout) => total + amout)
 
-  bill.tvac = (bill.htva * 0.21) + bill.htva
-  bill.tva = TVA
-  bill.date = {
+  const tvac = htva * 1.21
+  const tva = tvac - htva
+
+  const date = {
     year: new Date().getFullYear(),
     month: new Date().getMonth() + 3,
     day: new Date().getDay()
   }
+
+  const bill = { customer, services, htva, tvac, tva, date }
 
   const storage = localStorage.getItem('saleBills')
     ? JSON.parse(localStorage.getItem('saleBills'))
