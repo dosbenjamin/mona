12f7e85d171600bfec6de19814835167e1e070a7
dosbenjamin <dosbenjamin@gmail.com>
2021-02-09 21:17:44 +0100

    Add title removing when bill is the last

--- src/js/events/click/removeBill.js
+++ src/js/events/click/removeBill.js
@@ -1,10 +1,22 @@
+import { $ } from '../../utilities/dom'
+
 export default () => {
   const $bill = event.target.closest('.js-bill-item')
   const { id } = $bill.dataset
 
   const storage = JSON.parse(localStorage.getItem('saleBills'))
-    .filter((bill, index) => index !== parseFloat(id))
 
+  const newStorage = storage
+    .filter((bill, index) => index !== parseInt(id))
+
+  const current = storage[id]
+  const { date } = current
+
+  const isNotLast = newStorage
+    .map(({ date }) => date)
+    .some(({ year, month }) => year === date.year && month === date.month)
+
+  !isNotLast && $(`.js-title-row[data-year="${ date.year }"][data-month="${ date.month }"]`).remove()
   $bill.remove()
-  localStorage.setItem('saleBills', JSON.stringify(storage))
+  localStorage.setItem('saleBills', JSON.stringify(newStorage))
 }
