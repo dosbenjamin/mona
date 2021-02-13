0704756844bf1634cfac6072107bb2dde90a470f
dosbenjamin <dosbenjamin@gmail.com>
2021-02-13 22:09:23 +0100

    Fix bills triming per date

--- src/js/pages/Bill.js
+++ src/js/pages/Bill.js
@@ -1,6 +1,5 @@
 import Page from './Page'
 import { $, $$ } from '../utilities/dom'
-import 'regenerator-runtime/runtime'
 
 const months = {
   1: 'Janvier',
@@ -17,47 +16,36 @@ const months = {
   12: 'Décembre'
 }
 
-const waitTitle = (year, month) => new Promise((resolve, reject) => setTimeout(() => {
-  const noTitle = $(`.js-title-row[data-year="${year}"][data-month="${month}"]`) === null
-  noTitle ? reject(true) : resolve(false)
-}, 100))
-
-const createTitleRow = (year, month) => {
+const createTitleRow = ({ year, month }) => {
   const $row = document.createElement('template')
-  $row.innerHTML = `<tr class="js-title-row relative" data-year="${year}" data-month="${month}">
-    <td colspan="5" class="uppercase py-8">${months[month]} ${year}</td>
+  $row.innerHTML = `<tr class="js-title-row relative" data-year="${ year }" data-month="${ month }">
+    <td colspan="5" class="uppercase py-8">${ months[month] } ${ year }</td>
   </tr>`
   return $row.content
 }
 
-const createNewRow = async ({ infos, tvac, htva, tva, date }, index) => {
-  const { year, month } = date
-  const { company, firstname, lastname } = infos
-
+const createNewRow = ({
+  infos: { company, firstname, lastname },
+  date: { year, month },
+  tvac,
+  htva,
+  tva
+  }, index) => {
   const $row = document.createElement('template')
-
-  try {
-    await waitTitle(year, month)
-  } catch(error) {
-    const $titleRow = createTitleRow(year, month)
-    $row.content.prepend($titleRow)
-  } finally {
-    $row.innerHTML += `<tr class="relative js-bill-item" data-id="${ index }">
-      <td>${ index + 1 }</td>
-      <td>${ company ? company : (firstname + ' ' + lastname) }</td>
-      <td>${ htva.toFixed(2) }</td>
-      <td>${ tvac.toFixed(2) }</td>
-      <td>${ tva.toFixed(2) }</td>
-      <td class="relative">
-        <button class="absolute -right-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-grey-400 rounded-full w-12 h-12 overflow-hidden" type="button" data-click="removeBill">
-          <span class="line transform -translate-x-1/2 -translate-y-1/2 rotate-45"></span>
-          <span class="sr-only">Supprimer</span>
-          <span class="line transform -translate-x-1/2 -translate-y-1/2 -rotate-45"></span>
-        </button>
-      </td>
-    </tr>`
-  }
-
+  $row.innerHTML += `<tr class="relative js-bill-item" data-id="${ index }">
+    <td>${ index + 1 }</td>
+    <td>${ company ? company : (firstname + ' ' + lastname) }</td>
+    <td>${ htva.toFixed(2) }</td>
+    <td>${ tvac.toFixed(2) }</td>
+    <td>${ tva.toFixed(2) }</td>
+    <td class="relative">
+      <button class="absolute -right-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-grey-400 rounded-full w-12 h-12 overflow-hidden" type="button" data-click="removeBill">
+        <span class="line transform -translate-x-1/2 -translate-y-1/2 rotate-45"></span>
+        <span class="sr-only">Supprimer</span>
+        <span class="line transform -translate-x-1/2 -translate-y-1/2 -rotate-45"></span>
+      </button>
+    </td>
+  </tr>`
   return $row.content
 }
 
@@ -72,15 +60,25 @@ export default class extends Page {
     console.log('Action: Create')
   }
 
-  async list() {
+  list() {
+    const $area = $('.js-bill-area')
+
     const bills = localStorage.getItem(self.type)
       && JSON.parse(localStorage.getItem(self.type))
 
-    const $area = $('.js-bill-area')
+    const dates = bills
+      .map(({ date }) => date)
+      .reduce((unique, date) => {
+        return JSON.stringify(unique).includes(JSON.stringify(date))
+          ? unique
+          : [...unique, date]
+      }, []).reverse()
+      .forEach(date => $area.append(createTitleRow(date)))
 
-    const promises = bills && bills.map(createNewRow).reverse()
-    const $rows = await Promise.all(promises)
-
-    bills && $rows.forEach($row => $area.append($row))
+    bills && bills.forEach((bill, index) => {
+      const { date: { year, month } } = bill
+      const $row = createNewRow(bill, index)
+      $area.insertBefore($row, $(`.js-title-row[data-year="${ year }"][data-month="${ month }"]`).nextSibling)
+    })
   }
 }
