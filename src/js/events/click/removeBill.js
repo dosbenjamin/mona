40f2ed502f26de1c2fe346e1227b2c397969e7f3
dosbenjamin <dosbenjamin@gmail.com>
2021-02-14 11:10:45 +0100

    Fix bill removing with id

--- src/js/events/click/removeBill.js
+++ src/js/events/click/removeBill.js
@@ -1,20 +1,19 @@
-import { $ } from '../../utilities/dom'
-
 export default () => {
   const $bill = event.target.closest('.js-bill-item')
-  const { id } = $bill.dataset
+  const domId = $bill.dataset.id
 
   const storage = JSON.parse(localStorage.getItem(self.type))
   const newStorage = storage
-    .filter((bill, index) => index !== parseInt(id))
+    .filter(({ id }) => id !== domId)
 
-  const { date } = storage[id]
+  const { date } = storage
+    .find(({ id }) => id === domId)
 
   const isNotLast = newStorage
     .map(({ date }) => date)
     .some(({ year, month }) => year === date.year && month === date.month)
 
-  $bill.remove()
-  // !isNotLast && $bill.previousElementSibling.remove()
   localStorage.setItem(self.type, JSON.stringify(newStorage))
+  !isNotLast && $bill.previousElementSibling.remove()
+  $bill.remove()
 }
