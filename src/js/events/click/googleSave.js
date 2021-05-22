4ab52729715fdcb71f9007c2509bb44116024f50
dosbenjamin <dosbenjamin@gmail.com>
2021-05-22 12:22:06 +0200

    add save to google drive

--- src/js/events/click/googleSave.js
+++ src/js/events/click/googleSave.js
@@ -5,22 +5,33 @@ import { getGoogleStatus } from '../../components/googleAuthentification'
 const save = async () => {
   const file = getStorage()
   const isSignedIn = getGoogleStatus()
-  const request = isSignedIn && await gapi.client.request({
-    path: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: `'title': 'hello' ${JSON.stringify(file)}`
-      // data:
-      // 'mimeType': 'application/json',
-    // resource: {
-    //   'name': setJsonName(),
-    //   data: ,
-    // }
-  })
-}
+  // const request = isSignedIn && await gapi.client.request({
+  //   path: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart',
+  //   method: 'POST',
+  //   body: JSON.stringify(file, {
 
+  //     'title': setJsonName() + '.json'
+  //   })
+  // })
+
+
+  const request = isSignedIn && await gapi.client.drive.files.create({
+    resource: {
+      'name': setJsonName(),
+    },
+    media: {
+      mimeType: 'application/json',
+      body: JSON.stringify(file)
+    }
+  })
+
+  // await gapi.client.request({
+  //   path: 'https://www.googleapis.com/upload/drive/v3/files/' + request.result.id,
+  //   uploadType: 'media',
+  //   method: 'PUT',
+  //   body: JSON.stringify(file)
+  // })
+}
 
 export default () => {
   event.preventDefault()
